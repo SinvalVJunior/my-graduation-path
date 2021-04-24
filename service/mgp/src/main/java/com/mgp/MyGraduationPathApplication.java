ba984fc28ced59b7f8f5681c21eff3f861865e8c
Sinval <sinvalvieirajunior@gmail.com>
2021-04-24 11:07:27 -0300

    Adding spring boot starter project

---
+++ service/mgp/src/main/java/com/mgp/MyGraduationPathApplication.java
@@ -0,0 +1,13 @@
+package com.mgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGraduationPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGraduationPathApplication.class, args);
+	}
+
+}
---
+++ service/mgp/src/test/java/com/mgp/MyGraduationPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGraduationPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
