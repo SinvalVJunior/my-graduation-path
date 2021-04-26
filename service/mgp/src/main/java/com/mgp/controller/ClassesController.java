963233269730421003e9a20d7502d903d45b4102
Sinval <sinvalvieirajunior@gmail.com>
2021-04-25 23:07:21 -0300

    Adding swagger documentation

--- service/mgp/src/main/java/com/mgp/controller/ClassesController.java
+++ service/mgp/src/main/java/com/mgp/controller/ClassesController.java
@@ -10,6 +10,8 @@ import com.mgp.service.ClassesService;
 import com.mgp.service.dto.ClassCreateDTO;
 import com.mgp.service.dto.ClassGetDTO;
 import com.mgp.service.dto.ClassUpdateDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Api(tags = {"Classes"})
 @RequestMapping("/api/classes")
 @RestController
 public class ClassesController {
@@ -40,6 +43,7 @@ public class ClassesController {
         this.classesUpdateMapper = new ClassesUpdateMapper();
     }
 
+    @ApiOperation(value = "Retrieve all classes")
     @GetMapping
     public ResponseEntity<List<ClassGetModel>> getClasses() {
 
@@ -55,6 +59,7 @@ public class ClassesController {
         }
     }
 
+    @ApiOperation(value = "Create a class")
     @PostMapping
     public ResponseEntity<ClassGetModel> addClass(@Valid @RequestBody ClassCreateModel classCreateModel, BindingResult result) {
 
@@ -76,6 +81,8 @@ public class ClassesController {
         }
     }
 
+
+    @ApiOperation(value = "Update a class")
     @PatchMapping("/{id}")
     public ResponseEntity<ClassGetModel> updateClass(@PathVariable("id") @NotBlank Long id, @Valid @RequestBody ClassUpdateModel classUpdateModel, BindingResult result) {
         try {
@@ -100,6 +107,7 @@ public class ClassesController {
         }
     }
 
+    @ApiOperation(value = "Delete a class")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteClass(@PathVariable("id") Long id) {
         try {
