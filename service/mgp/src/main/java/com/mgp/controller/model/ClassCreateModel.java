8a1e82f0e959a7bfe7388e7a7098e4917b2d43a8
Sinval <sinvalvieirajunior@gmail.com>
2021-04-26 12:07:11 -0300

    Refactoring parameters contants class name

--- service/mgp/src/main/java/com/mgp/controller/model/ClassCreateModel.java
+++ service/mgp/src/main/java/com/mgp/controller/model/ClassCreateModel.java
@@ -1,6 +1,6 @@
 package com.mgp.controller.model;
 
-import com.mgp.constants.ParametersConstants;
+import com.mgp.constants.ModelsConstants;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,16 +14,16 @@ public class ClassCreateModel {
 
     @NotNull
     @Size(
-            min = ParametersConstants.Classes.NAMES_MIN_SIZE,
-            max = ParametersConstants.Classes.NAMES_MAX_SIZE,
-            message = "Name accepts at least " + ParametersConstants.Classes.NAMES_MIN_SIZE
-                    + " characters and at most " + ParametersConstants.Classes.NAMES_MAX_SIZE + " character(s)."
+            min = ModelsConstants.Classes.NAMES_MIN_SIZE,
+            max = ModelsConstants.Classes.NAMES_MAX_SIZE,
+            message = "Name accepts at least " + ModelsConstants.Classes.NAMES_MIN_SIZE
+                    + " characters and at most " + ModelsConstants.Classes.NAMES_MAX_SIZE + " character(s)."
     )
     private String name;
 
     @NotNull
-    @Min(value = ParametersConstants.Classes.HOURS_MIN_SIZE, message = "Hours must be at least " + ParametersConstants.Classes.HOURS_MIN_SIZE)
-    @Max(value = ParametersConstants.Classes.HOURS_MAX_SIZE, message = "Hours must be at most " + ParametersConstants.Classes.HOURS_MAX_SIZE)
+    @Min(value = ModelsConstants.Classes.HOURS_MIN_SIZE, message = "Hours must be at least " + ModelsConstants.Classes.HOURS_MIN_SIZE)
+    @Max(value = ModelsConstants.Classes.HOURS_MAX_SIZE, message = "Hours must be at most " + ModelsConstants.Classes.HOURS_MAX_SIZE)
     private Integer hours;
 
     @NotNull
